991302b749c1a48b08ad6710663e409fbbb38e8f
박찬도 <cantao@kakao.com>
2023-09-06 08:25:27 +0900

    플랫폼 파일 상속으로 변경

---
+++ Targets/CDSheetRouter/Sources/MovingSheetOperator.swift
@@ -0,0 +1,24 @@
+//
+//  MovingSheetOperator.swift
+//  CDSheetRouter
+//
+//  Created by Littlefox iOS Developer on 2023/09/06.
+//
+
+import SwiftUI
+
+public class MovingSheetOperator<SheetRouter:SheetRouterProtocol>: ObservableObject {
+    @Published public var sheets: [SheetRouterContext<SheetRouter>] = []
+    //View의 화면 이동
+    public func go(_ sheet: SheetRouter?, animation: SheetAnimation){
+        if let s = sheet{
+            self.sheets.append(SheetRouterContext(router: s, animation: animation))
+        }
+    }
+}
+
+public extension View {
+    func routering<SheetRouter: SheetRouterProtocol>(_ sheets: Binding<[SheetRouterContext<SheetRouter>]>) -> some View {
+        modifier(MovingRoute(sheets: sheets))
+    }
+}

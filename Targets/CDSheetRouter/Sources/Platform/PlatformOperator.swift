0efe1dd64b79a3aacf75d217b75a65de7f1694ff
정문희 <jg.moonhee@littlefox.com>
2023-10-26 08:50:27 +0900

    platform 구조 수정 테스트

--- Targets/CDSheetRouter/Sources/Platform/PlatformOperator.swift
+++ Targets/CDSheetRouter/Sources/Platform/PlatformOperator.swift
@@ -7,10 +7,11 @@
 
 import SwiftUI
 
-
-
 public class PlatformOperator<SheetRouter:SheetRouterProtocol, VM: PlatformOperatorVM_P>: MovingSheetOperator<SheetRouter> {
-    public weak var viewModel: VM?
+//public class PlatformOperator<SheetRouter:SheetRouterProtocol>: MovingSheetOperator<SheetRouter> {
+    
+//    public weak var viewModel: VM?
+    @Published public var viewModel: VM?
     
     public init(viewModel: VM) {
         self.viewModel = viewModel
@@ -23,4 +24,19 @@ public class PlatformOperator<SheetRouter:SheetRouterProtocol, VM: PlatformOpera
 }
 
 
-
+//
+//public class PlatformOperator<SheetRouter:SheetRouterProtocol, VM: PlatformOperatorVM_P>: MovingSheetOperator<SheetRouter> {
+//    public weak var viewModel: VM?
+//    
+//    public init(viewModel: VM) {
+//        self.viewModel = viewModel
+//    }
+//
+//    //View의 이벤트를 받아 ViewMdoel로 보냄
+//    public func send(_ event: VM.Event){
+//        self.viewModel?.received(event: event)
+//    }
+//}
+//
+//
+//

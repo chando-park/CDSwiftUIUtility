beca82699e025c048bb96fad63d5c767b774d8df
박찬도 <cantao@kakao.com>
2024-03-19 09:08:36 +0900

    3/19

--- Targets/CDNavigation/Sources/CDNavigationController.swift
+++ Targets/CDNavigation/Sources/CDNavigationController.swift
@@ -304,16 +304,15 @@ public class CDNavigationController: UINavigationController {
                 return
             }
             
-//            guard let image = self.closeImage else{
-//                self.closeBtn?.isHidden = true
-//                return
-//            }
+            guard let image = self.closeImage else{
+                return
+            }
             
             closeBtn.isEnabled = false
             UIView.animate(withDuration: 0.15) {
                 closeBtn.frame.origin.x = self.view.frame.size.width
             } completion: { _ in
-                closeBtn.setImage(self.closeImage, for: .normal)
+                closeBtn.setImage(image, for: .normal)
                 if self.isCloseBtnHidden == false {
                     UIView.animate(withDuration: 0.15) {
                         let left = closeBtn.frame.size.width*(42.0/110)
@@ -526,6 +525,14 @@ public class CDNavigationController: UINavigationController {
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        self.naviBar?.frame = CGRect(origin: CGPoint(x: 0, y: self.statusBarHeight), size: CGSize(width: self.view.frame.size.width, height: navigationBarHeight))
+        let type = self.navigationBarTitleType
+        self.navigationBarTitleType = type
     }
 
     @objc private func backCallback(_ sender: UIButton){
